c950cf628505ffe20a8b01a0d62110007fc087b1
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-01-04 17:01:21 +0600

    Add some tools

--- flatpak_install.sh
+++ flatpak_install.sh
@@ -3,10 +3,18 @@
 # Update package lists
 sudo apt update
 
+sudo apt upgrade -y
+
+sudo nala install gnome-tweaks -y
+
+sudo nala install gnome-shell-extensions -y
+
 # Install Flatpak and Gnome Software plugin
-sudo apt install flatpak gnome-software-plugin-flatpak
+sudo nala install flatpak gnome-software-plugin-flatpak
 
 # Add Flathub repository
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 flatpak install flathub org.mozilla.firefox
+
+sudo cp -r $HOME/Flathub/gitconfig $HOME
