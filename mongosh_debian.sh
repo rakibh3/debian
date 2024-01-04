2a6408af5311dbcca36d300130ac27f898dbf3f9
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-01-04 19:57:44 +0600

    Fix Mongosh instalation script

--- mongosh_debian.sh
+++ mongosh_debian.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+sudo nala install gnupg
+
 # Import the MongoDB GPG key
-wget -qO - https://www.mongodb.org/static/pgp/server-7.0.asc | sudo apt-key add -
+wget -qO- https://www.mongodb.org/static/pgp/server-7.0.asc | sudo tee /etc/apt/trusted.gpg.d/server-7.0.asc
 
 # Create a list file for MongoDB
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/debian $(lsb_release -cs)/mongodb-org/7.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/debian bullseye/mongodb-org/7.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 
 # Update the package list
 sudo nala update
