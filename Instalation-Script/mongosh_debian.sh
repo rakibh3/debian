88808a2af3bde253338a2a14db604d6b61ae5363
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-01-05 21:19:57 +0600

    Mongosh installer

--- Instalation-Script/mongosh_debian.sh
+++ Instalation-Script/mongosh_debian.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-sudo nala install gnupg curl
 
-# Import the MongoDB GPG key
-#curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
- #  sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
-  # --dearmor
+sudo nala install gnupg
 
-curl -fsSL https://pgp.mongodb.com/server-7.0.asc |sudo gpg  --dearmor -o /etc/apt/trusted.gpg.d/mongodb-server-7.0.gpg
+#wget -qO- https://www.mongodb.org/static/pgp/server-7.0.asc | sudo tee /etc/apt/trusted.gpg.d/server-7.0.asc
+
+curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+   --dearmor
+
+echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] http://repo.mongodb.org/apt/debian bullseye/mongodb-org/7.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 
 
-# Create a list file for MongoDB
-#echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] http://repo.mongodb.org/apt/debian bullseye/mongodb-org/7.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 
-# Update the package list
 sudo nala update
 
-# Install the MongoDB shell
+sudo nala install -y mongodb-mongosh-shared-openssl3
+
 sudo nala install -y mongodb-org
