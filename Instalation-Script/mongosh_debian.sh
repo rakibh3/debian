711673c2d2b330c40f4bf1d04d0169a0a6ab63aa
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-01-08 20:23:05 +0600

    Mongosh installer Testing

--- Instalation-Script/mongosh_debian.sh
+++ Instalation-Script/mongosh_debian.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-
 sudo nala install gnupg
-
-#wget -qO- https://www.mongodb.org/static/pgp/server-7.0.asc | sudo tee /etc/apt/trusted.gpg.d/server-7.0.asc
 
 curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
    sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
@@ -11,10 +8,6 @@ curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
 
 echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] http://repo.mongodb.org/apt/debian bullseye/mongodb-org/7.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 
-
-
 sudo nala update
-
-sudo nala install -y mongodb-mongosh-shared-openssl3
 
 sudo nala install -y mongodb-org
