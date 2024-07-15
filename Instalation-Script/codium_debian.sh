8428edea14b3de348d1d24ddf8a7fae6364f9ee5
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-07-15 23:27:14 +0600

    Fixed flathub

--- Instalation-Script/codium_debian.sh
+++ Instalation-Script/codium_debian.sh
@@ -8,4 +8,4 @@ wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.g
 echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
     | sudo tee /etc/apt/sources.list.d/vscodium.list
 
-sudo apt update && sudo apt install codium
+sudo nala update && sudo nala install codium
