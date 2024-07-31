55bece90ce9c5e276f5ad9caaf77a33551f08037
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-07-31 23:35:22 +0600

    install_common_packages

---
+++ Instalation-Script/install_common_packages.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Update package lists
+echo "Updating package lists..."
+sudo nala update
+
+# Upgrade installed packages
+echo "Upgrading installed packages..."
+sudo nala upgrade -y
+
+# Install common dependencies
+echo "Installing common packages..."
+sudo nala install -y \
+    curl \
+    wget \
+    gpg \
+    gnupg \
+    apt-transport-https \
+    ca-certificates \
+    gnome-software-plugin-flatpak \
+    flatpak \
+    nala \
+    git
+
+echo "Common packages installation complete."
