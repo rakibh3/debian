0e6926017320f97c68ecb2db85a6e78313607291
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-08-20 22:27:36 +0600

    Common Packages

--- Instalation-Script/install_common_packages.sh
+++ Instalation-Script/install_common_packages.sh
@@ -5,10 +5,6 @@ echo "Updating package lists..."
 sudo apt update && apt upgrade -y
 sudo apt install nala
 
-# Update package lists with nala
-echo "Updating package lists with nala..."
-sudo nala update && mala upgrade -y
-
 # Install common dependencies
 echo "Installing common packages..."
 sudo nala install -y \
